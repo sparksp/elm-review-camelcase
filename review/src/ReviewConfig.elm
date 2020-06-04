661b1bd10a9c05f6b5c4b7497bad56c324cee9b3
Phill Sparks <me@phills.me.uk>
2020-06-04 21:10:01 +0100

    Add Documentation.ReadmeLinksPointToCurrentVersion

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,16 +1,6 @@
 module ReviewConfig exposing (config)
 
-{-| Do not rename the ReviewConfig module or the config function, because
-`elm-review` will look for these.
-
-To add packages that contain rules, add them to this review project using
-
-    `elm install author/packagename`
-
-when inside the directory containing this file.
-
--}
-
+import Documentation.ReadmeLinksPointToCurrentVersion
 import NoDebug.Log
 import NoDebug.TodoOrToString
 import NoUnused.CustomTypeConstructors
@@ -24,7 +14,8 @@ import UseCamelCase
 
 config : List Rule
 config =
-    [ NoDebug.Log.rule
+    [ Documentation.ReadmeLinksPointToCurrentVersion.rule
+    , NoDebug.Log.rule
     , NoDebug.TodoOrToString.rule
     , NoUnused.CustomTypeConstructors.rule []
     , NoUnused.Dependencies.rule
